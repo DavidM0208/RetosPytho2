8943abe9ff3b0ecc6b9625903da6b678f1386944
DavidM0208 <sdcifuentes85@misena.edu.co>
2023-06-10 17:08:59 -0500

    Se añadio el reto 3, hecho con funciones

--- Reto4/Reto3.0.py
+++ Reto4/Reto3.0.py
@@ -4,37 +4,41 @@ instructores2557861 = {}
 def agregarModificar():
 
         inst = input("Ingrese el nombre del instructor: \n")
+        inst = inst.lower()
         if inst in instructores2557861:
             print("El instructor ya se encuentra registrado en la ficha.")
             opcion1 = int(input("Digite 1 si desea modificar nombre del instructor. \nDigite 2 si desea modificar la materia del instructor de la ficha. \nDigite 3 si desea modificar el teléfono del instructor. \n"))
             if opcion1 == 1:
                 nombre = input("Ingrese el nuevo nombre del instructor. \n")
                 instructores2557861 ['Nombre'] = nombre
+                print(instructores2557861)
             elif opcion1 == 2:
                 materia = input("Ingrese la nueva materia del instructor. \n")
                 instructores2557861 [inst]['Materia'] = materia
+                print(instructores2557861)
             elif opcion1 == 3:
                 telefono = int(input("Ingrese el nuevo teléfono del instructor. \n"))
                 instructores2557861 [inst]['Teléfono'] = telefono
+                print(instructores2557861)
             else:
                 print("¡¡ERROR!! Opción no valida.")
         
         else:
             print("El instructor no se encuentra en el sistema.")
             instructores2557861[inst] = {}
-            materia = input("Ingrese la nueva materia del instructor. \n")
+            materia = input("Ingrese la materia del instructor. \n")
             instructores2557861[inst]['Materia'] = materia
-            telefono = int(input("Ingrese el nuevo teléfono del instructor. \n"))
+            telefono = int(input("Ingrese el teléfono del instructor. \n"))
             instructores2557861[inst]['Telefono'] = telefono
             print(instructores2557861)
 
 def buscar():
 
-        ins = input("Ingrese el nombre del instructor que desea buscar: ")
+        inst = input("Ingrese el nombre del instructor que desea buscar: ")
         inst = inst.lower()
         print("Se encontraron los siguientes resultados:")
-        for ins in instructores2557861.items():
-            if inst.startswith(ins):
+        for inst, datos in instructores2557861.items():
+            if inst.startswith(inst):
                 print(inst)
                 print(f"Materia: {instructores2557861[inst]['Materia']} \nNúmero de teléfono: {instructores2557861[inst]['Telefono']}")
             else:
@@ -46,7 +50,7 @@ def borrar():
         nombre = nombre.lower()
         if nombre in instructores2557861:
             print("Se encontraron los siguientes resultados: ")
-            print(instructores2557861)
+            print(f"{instructores2557861}")
             borrar = int(input("Digite 1 para borrar el instructor. \nDigite 2 para cancelar la acción. \n"))
             if borrar == 1:
                 del instructores2557861[nombre]
@@ -59,9 +63,8 @@ def borrar():
                 print(f"El instructor {nombre} no existe en el diccionario.")
 
 def listar():
-
-        for x in instructores2557861:
-            print(f"Los instructores registrados son {x} con la materia {instructores2557861[x]['Materia']} y un número de telefono {instructores2557861[x]['Telefono']}")
+    for x in instructores2557861:
+        print(f"Los instructores registrados son {x} con la materia {instructores2557861[x]['Materia']} y un número de telefono {instructores2557861[x]['Telefono']}")
 
 while op == 1:
     print("¡¡Bienvenido!!")
@@ -82,5 +85,6 @@ while op == 1:
         listar()
     elif opcion == 5:
         print("Gracias por usar nuestros servicios.")
+        break
 
-    op = int(input("1. Para seguir modificando el diccionario. \n2. Para salir."))
+    op = int(input("1. Para seguir modificando el diccionario. \n2. Para salir. \n"))
